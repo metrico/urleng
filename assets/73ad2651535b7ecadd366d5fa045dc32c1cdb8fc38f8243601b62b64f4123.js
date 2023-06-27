b60d0aeae834feaad72c9f8e16a3e32a062eaeb2
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-06-27 19:33:56 +0200

    assets

---
+++ assets/73ad2651535b7ecadd366d5fa045dc32c1cdb8fc38f8243601b62b64f4123.js
@@ -0,0 +1,1 @@
+!function(){"use strict";const e=Array.from(document.getElementsByTagName("img")),t=e=>{const t=e.parentElement;t&&t.style.removeProperty("animation")};e.forEach((e=>{e.complete?t(e):e.addEventListener("load",(()=>{t(e)}))}))}();
